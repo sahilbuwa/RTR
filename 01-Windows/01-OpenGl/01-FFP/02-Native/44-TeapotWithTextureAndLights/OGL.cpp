f92705502d4d64b41312004e9dae3597d44dbe93
Sahil Buwa <sahilbuwa99@gmail.com>
2022-06-24 02:14:22 +0530

    Teapot Done

---
+++ 01-Windows/01-OpenGl/01-FFP/02-Native/44-TeapotWithTextureAndLights/OGL.cpp
@@ -0,0 +1,494 @@
+// Header files
+#include<windows.h> //SDK path madhli header file declare karaichi padhhat
+#include<stdio.h> // For FileIO()
+#include<stdlib.h> // For Exit()
+#define _USE_MATH_DEFINES
+#include<math.h>
+
+// OpenGL header files
+#include<GL/gl.h>
+#include<GL/glu.h>
+
+//Personal Libraries
+#include"OGL.h"  //Teapot sathi
+#include"OGL1.h" //Aplya path (local) madhli header file declare karaichi padhhat
+
+// OpenGL Libraries
+#pragma comment(lib,"OpenGL32.lib") 
+#pragma comment(lib,"GLU32.lib") 
+
+// Defines
+#define WIN_WIDTH 800
+#define WIN_HEIGHT 600
+
+// Global Variable Declarations
+HWND ghwnd=NULL;
+HDC ghdc=NULL;
+HGLRC ghrc=NULL;
+BOOL gbFullScreen=FALSE;
+FILE *gpFile=NULL;
+BOOL gbActiveWindow=FALSE;
+float angle = 0.0f;
+GLfloat lightAmbient[] = {0.5f, 0.5f, 0.5f, 1.0f};
+GLfloat lightDiffuse[] = {1.0f, 1.0f, 1.0f, 1.0f};
+GLfloat lightPosition[] = {0.0f, 0.0f, 2.0f, 1.0f};
+BOOL gbLight = FALSE;
+BOOL bTexture = FALSE;
+GLuint texture_marble;
+
+// Global Function Declarations
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+// Entry-Point Function
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+    // Function Declarations
+    int initialize(void);
+    void display(void);
+    void update(void);
+    void uninitialize(void);
+
+    // Variable Declarations
+    WNDCLASSEX wndclass;
+    HWND hwnd;
+    MSG msg;
+    TCHAR szAppName[]= TEXT("MaziWindow");
+    BOOL bDone=FALSE;
+    int iRetval=0;
+
+    // Code
+    if(fopen_s(&gpFile,"Log.txt","w")!=0)// fopen_s(File Open Secured)
+    {
+        MessageBox(NULL,TEXT("Creation of log file failed. Exiting...\n"),TEXT("FileIO Error"),MB_OK);
+        exit(0);
+    }
+    else
+    {
+        fprintf(gpFile,"Log File Is Successfully Created. \n");
+    }
+
+    // Initialization of WNDCLASSEX structure
+    wndclass.cbSize = sizeof(WNDCLASSEX);
+    wndclass.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wndclass.cbClsExtra = 0;
+    wndclass.cbWndExtra = 0;
+    wndclass.lpfnWndProc = WndProc;
+    wndclass.hInstance = hInstance;
+    wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH); // BLACK_BRUSH
+    wndclass.hIcon = LoadIcon( hInstance, MAKEINTRESOURCE(MYICON)); //ID of icon , MYICON int aslyane tyala string madhe convert karnyasathi MAKEINTRESOURCE macro waparaava laagto
+    wndclass.hCursor = LoadCursor(NULL , IDC_ARROW); // ID of cursor
+    wndclass.lpszClassName = szAppName;
+    wndclass.lpszMenuName = NULL;
+    wndclass.hIconSm = LoadIcon( hInstance, MAKEINTRESOURCE(MYICON)); //Chotu Icon sathi 
+
+    // Register The WNDCLASSEX
+    RegisterClassEx(&wndclass);
+
+    // Create The Window
+    hwnd = CreateWindowEx ( WS_EX_APPWINDOW,
+                            szAppName,
+                            TEXT("Sahil Ajit Buwa - OGL window"),
+                            WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE,
+                            (GetSystemMetrics(SM_CXSCREEN)-WIN_WIDTH)/2, 
+                            (GetSystemMetrics(SM_CYSCREEN)-WIN_HEIGHT)/2,
+                            WIN_WIDTH,
+                            WIN_HEIGHT,
+                            NULL,
+                            NULL, //Menu Handle , refer 28
+                            hInstance, 
+                            NULL // Extra Maahiti dya ki
+                        );
+
+    ghwnd=hwnd;
+    // Initialize
+    iRetval=initialize();
+    if(iRetval==-1)
+    {
+        fprintf(gpFile,"Choose pixel format failed.\n");
+        uninitialize();
+    }
+    else if(iRetval==-2)
+    {
+        fprintf(gpFile,"Set pixel format failed.\n");
+        uninitialize();
+    }
+
+    else if(iRetval==-3)
+    {
+        fprintf(gpFile,"Create OpenGL context failed.\n");
+        uninitialize();
+    }
+
+    else if(iRetval==-4)
+    {
+        fprintf(gpFile,"Making OpenGL context as current context failed.\n");
+        uninitialize();
+    }
+    else
+    {
+        fprintf(gpFile,"OpenGL context setting successfull.\n");
+    }
+
+    // Show Window
+    ShowWindow(hwnd, iCmdShow  ); // SW_SHOWNORMAL
+    // SW_MAXIMIZE |SW_MINIMIZE |SW_HIDE (2nd parameter)
+    // Window default kashi dakhav te OS che behaviour function cha 4th parameter
+
+    // Foregrounding and focusing the window
+    SetForegroundWindow(hwnd); // Both(hwnd,ghwnd) will do good. 
+    SetFocus(hwnd);
+
+    // Game Loop
+    while(bDone==FALSE)
+    {
+        if(PeekMessage(&msg,NULL,0,0,PM_REMOVE))
+        {
+            if(msg.message==WM_QUIT)
+                bDone=TRUE;
+            else
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        }
+        else
+        {
+            if(gbActiveWindow==TRUE)
+            {
+                // Render the scene
+                display();
+
+                // Update the scene
+                update();
+
+            }
+        }
+    }
+    uninitialize();
+    return (int)msg.wParam; 
+}
+
+// Callback Function
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    // Function Declarations
+    void ToggleFullScreen(void);
+    void resize(int,int);
+
+    // Variable Declarations
+    TCHAR str[255];
+
+    // Code
+    switch(iMsg)
+    {
+        case WM_SETFOCUS:
+            gbActiveWindow=TRUE;
+            break;
+        case WM_KILLFOCUS:
+            gbActiveWindow=FALSE;
+            break;
+        case WM_ERASEBKGND:
+            return 0;
+        // As this is retained mode graphics, there is WM_PAINT to paint.    
+        case WM_CHAR:
+            switch(wParam)
+            {
+                case 'F':
+                case 'f':
+                    ToggleFullScreen();
+                    break;
+                case 'L':
+                case 'l':
+				    if (gbLight == FALSE)
+				    {
+				        glEnable(GL_LIGHTING);
+				        gbLight = TRUE;
+				    }
+				    else
+				    {
+				        glDisable(GL_LIGHTING);
+				        gbLight = FALSE;
+				    }
+				    break;
+				case 't':
+				case 'T':
+				    if (bTexture == FALSE)
+				    {
+				        glEnable(GL_TEXTURE_2D);
+				        bTexture = TRUE;
+				    }
+				    else
+				    {
+				        glDisable(GL_TEXTURE_2D);
+				        bTexture = FALSE;
+				    }
+				    break;
+                default:
+                    break;
+            }
+            break;
+        case WM_KEYDOWN:
+            switch(wParam)
+            {
+                case 27:
+                    DestroyWindow(hwnd);
+                    break;
+                default:
+                    break;
+            }
+            break;
+        case WM_SIZE:
+            resize(LOWORD(lParam),HIWORD(lParam));
+            break;
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            break;
+    }
+
+    return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
+
+void ToggleFullScreen(void)
+{
+    // Variable Declarations
+    static DWORD dwStyle;
+    static WINDOWPLACEMENT wp;
+    MONITORINFO mi;
+
+    // Code
+    wp.length=sizeof(WINDOWPLACEMENT);
+
+    if(gbFullScreen==FALSE)
+    {
+        dwStyle=GetWindowLong(ghwnd,GWL_STYLE);
+        if(dwStyle & WS_OVERLAPPEDWINDOW)
+        {
+            mi.cbSize=sizeof(MONITORINFO);
+
+            if( GetWindowPlacement( ghwnd, &wp) && GetMonitorInfo( MonitorFromWindow( ghwnd, MONITORINFOF_PRIMARY), &mi))
+            {
+                SetWindowLong( ghwnd, GWL_STYLE, dwStyle & ~WS_OVERLAPPEDWINDOW );
+                SetWindowPos( ghwnd, HWND_TOP, mi.rcMonitor.left, mi.rcMonitor.top, mi.rcMonitor.right-mi.rcMonitor.left, mi.rcMonitor.bottom-mi.rcMonitor.top, SWP_NOZORDER | SWP_FRAMECHANGED); //WM_NCCalcSize non client area calculate kar ani consider kar
+            }    
+            ShowCursor(FALSE);
+            gbFullScreen=TRUE;
+        }
+    }
+    else
+    {
+        SetWindowLong(ghwnd, GWL_STYLE, dwStyle | WS_OVERLAPPEDWINDOW);
+        SetWindowPlacement( ghwnd, &wp);
+        SetWindowPos( ghwnd, HWND_TOP, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER | SWP_NOZORDER | SWP_FRAMECHANGED );
+
+        ShowCursor(TRUE);
+        gbFullScreen=FALSE;
+    }
+
+}
+
+int initialize(void)
+{
+    // Function Declarations
+    void resize(int,int);
+    BOOL LoadGLTexture(GLuint*,TCHAR[]);
+    void uninitialize(void);
+
+    // Variable Declarations
+    PIXELFORMATDESCRIPTOR pfd;
+    int iPixelFormatIndex=0;
+    
+    // Code
+    ZeroMemory(&pfd,sizeof(PIXELFORMATDESCRIPTOR));
+    // Initialization of PIXELFORMATDESCRIPTOR structure
+    pfd.nSize = sizeof(PIXELFORMATDESCRIPTOR);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 32;
+    pfd.cRedBits = 8;
+    pfd.cGreenBits = 8;
+    pfd.cBlueBits = 8;
+    pfd.cAlphaBits = 8;
+    pfd.cDepthBits = 32; // 24 pan chaltai
+    
+    // GetDC
+    ghdc = GetDC(ghwnd);
+
+    // Choose Pixel Format
+    iPixelFormatIndex = ChoosePixelFormat( ghdc, &pfd);
+    if(iPixelFormatIndex==0)
+        return -1;
+
+    // Set Chosen Pixel Format
+    if(SetPixelFormat(ghdc,iPixelFormatIndex,&pfd)==FALSE)
+        return -2;
+    
+    // Create OpenGL rendereing context
+    ghrc = wglCreateContext(ghdc);
+    if(ghrc==NULL)
+        return -3;
+
+    // Make the rendering context as current context
+    if(wglMakeCurrent(ghdc,ghrc)==FALSE)
+        return -4;
+
+    if(LoadGLTexture(&texture_marble, MAKEINTRESOURCE(IDBITMAP_MARBLE))==FALSE)
+    {
+        fprintf(gpFile,"LoadGLTexture for smiley Failed.\n");
+        uninitialize();
+        return -5;
+    }
+    // Here Starts OpenGL code
+    // Clear the screen using black color
+    glClearColor(0.0f,0.0f,0.0f,1.0f);
+
+    // Depth Related Changes
+    glClearDepth(1.0f);
+    glEnable(GL_DEPTH_TEST);
+    glDepthFunc(GL_LEQUAL);
+
+    glShadeModel(GL_SMOOTH);
+    glHint(GL_PERSPECTIVE_CORRECTION_HINT,GL_NICEST);
+    
+    // Light initialize
+    glLightfv(GL_LIGHT1, GL_AMBIENT, lightAmbient);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, lightDiffuse);
+    glLightfv(GL_LIGHT1, GL_POSITION, lightPosition);
+    glEnable(GL_LIGHT1);
+
+    // Warmup Resize Call
+    resize(WIN_WIDTH,WIN_HEIGHT);
+    return 0;
+}
+
+void resize(int width, int height)
+{
+    // Code
+    if(height==0)
+        height=1; // To avoid divided by 0 error(illegal statement) in future calls..
+
+    glViewport(0,0,(GLsizei)width,(GLsizei)height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
+
+}
+
+void display(void)
+{
+    // Variable Declarations
+    int vi, ni, ti;
+    int i, j;
+    // Code
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    
+    glTranslatef(0.0f,0.0f,-2.0f);
+    glRotatef((GLfloat)angle, 0.0f, 1.0f, 0.0f);
+    
+    if(bTexture == TRUE)
+    	glBindTexture(GL_TEXTURE_2D, texture_marble);
+    else
+    	glBindTexture(GL_TEXTURE_2D, 0);
+    
+    glBegin(GL_TRIANGLES);
+	for(i = 0; i < sizeof(face_indicies)/sizeof(face_indicies[0]); i++)
+	{
+		for(j = 0; j < 3; j++)
+		{
+			vi = face_indicies[i][j];
+			ni = face_indicies[i][j+3];
+			ti = face_indicies[i][j+6];
+			glNormal3f(normals[ni][0], normals[ni][1], normals[ni][2]);
+			glTexCoord2f(textures[ti][0],textures[ti][1]);
+			glVertex3f(vertices[vi][0], vertices[vi][1], vertices[vi][2]);
+		}
+	}
+	glEnd();
+    
+    SwapBuffers(ghdc);
+}
+
+void update(void)
+{
+    // Code
+    angle=angle+0.1f;
+    if(angle>=360.0f)
+        angle=angle-360.0f;
+
+}
+
+BOOL LoadGLTexture(GLuint *texture, TCHAR imageResourceID[])
+{
+    // Variable Declarations
+    HBITMAP hBitmap = NULL;
+    BITMAP bmp;
+    BOOL bResult = FALSE;
+    // Code
+    hBitmap = (HBITMAP)LoadImage(GetModuleHandle(NULL), imageResourceID, IMAGE_BITMAP, 0, 0, LR_CREATEDIBSECTION);
+    if(hBitmap)
+    {
+        bResult = TRUE;
+        GetObject(hBitmap, sizeof(bmp), &bmp);
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 4);
+        glGenTextures(1, texture);
+        glBindTexture(GL_TEXTURE_2D, *texture);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
+        // Create The Texture
+        gluBuild2DMipmaps(GL_TEXTURE_2D, 3, bmp.bmWidth, bmp.bmHeight, GL_BGR_EXT, GL_UNSIGNED_BYTE, bmp.bmBits);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        DeleteObject(hBitmap);
+
+    }
+    return bResult;
+}
+
+void uninitialize(void)
+{
+    // Function Declarations
+    void ToggleFullscreen(void);
+    // Code
+    if(gbFullScreen)
+    {
+        ToggleFullScreen();
+    }
+    if(wglGetCurrentContext()==ghrc)
+    {
+        wglMakeCurrent(NULL,NULL);
+    }
+    if(ghrc)
+    {
+        wglDeleteContext(ghrc);
+        ghrc=NULL;
+    }
+    if(ghdc)
+    {
+        ReleaseDC(ghwnd,ghdc);
+        ghdc=NULL;
+    }
+    if(ghwnd)
+    {
+        DestroyWindow(ghwnd);
+        ghwnd=NULL;
+    }
+    if(texture_marble)
+    {
+        glDeleteTextures(1, &texture_marble);
+    }
+    if(gpFile)
+    {
+        fprintf(gpFile,"Log File Is Successfully Closed.\n");
+        fclose(gpFile);
+        gpFile=NULL;
+    }
+}
